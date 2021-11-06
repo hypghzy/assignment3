99c0b448b9134b8e7ea16d5bb75fdf0b5acbb8ee
BruceHou <a1783922@student.adelaide.edu.au>
2021-11-06 21:34:36 +0800

    fully automated

--- test.sh
+++ test.sh
@@ -10,32 +10,38 @@
 #!/bin/bash
 
 echo "Start testing"
-java PublicServices
-timeArray=(0 0 0 0 0)
+for noUse in {1..10}; do
+    java PublicServices
+    timeArray=(0 0 0 0 0)
 
-for j in {1..6}; do
-    for k in 1 2 3 4 5; do
-        timestamp=$(date +%s%3N)
-        timeArray[$k]=$(expr $timestamp % 50)
+    for j in {1..6}; do
+        for k in 1 2 3 4 5; do
+            timestamp=$(date +%s%3N)
+            timeArray[$k]=$(expr $timestamp % 50)
+        done
+        for n in {0..49}; do
+            case $n in
+            ${timeArray[0]})
+                java Acceptor -1 &
+                ;;
+            ${timeArray[1]} | ${timeArray[2]} | ${timeArray[3]} | ${timeArray[4]})
+                java Acceptor 30 &
+                ;;
+            *)
+                java Acceptor 2 &
+                ;;
+            esac
+            # echo Acceptor $n
+        done
+        process_id=0
+        if test $j -gt 1; then
+            java Proposer $(expr $j \* 50)
+            echo "One round finished."
+        fi
+        sleep 3
     done
-    for n in {0..49}; do
-        case $n in
-        ${timeArray[0]})
-            java Acceptor -1 &
-            ;;
-        ${timeArray[1]} | ${timeArray[2]} | ${timeArray[3]} | ${timeArray[4]})
-            java Acceptor 30 &
-            ;;
-        *)
-            java Acceptor 2 &
-            ;;
-        esac
-        # echo Acceptor $n
-    done
-    process_id=0
-    if test $j -gt 1; then
-        java Proposer $(expr $j \* 50)
-        echo "One round finished."
-    fi
-    sleep 3
+
+    sleep 4
+    pkill -f java
+
 done
